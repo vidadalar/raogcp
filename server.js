b74bbb598646ed3da0f67b61dd2249022028a83f
vidadalar <42088202+vidadalar@users.noreply.github.com>
2018-08-04 10:51:40 +0100

    Updated

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var handleRequest = function(request, response) {
   response.writeHead(200);
-  response.end("Hello World from Edureka Team to Google Next Event");
+  response.end("Hello World from Rao with latest version");
 }
 var www = http.createServer(handleRequest);
 www.listen(8080);
